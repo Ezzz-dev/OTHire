556d352f35145e893e7ea1949392d05b83b8e9eb
Ezzz <alejandrodemujica@gmail.com>
2014-06-03 14:52:13 -0430

    Magic wall rune not decaying fix

--- data/spells/scripts/support/magic_wall_rune.lua
+++ data/spells/scripts/support/magic_wall_rune.lua
@@ -1,6 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
-setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1498)
+setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1497)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 
 function onCastSpell(cid, var)
