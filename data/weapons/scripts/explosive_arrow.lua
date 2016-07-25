9b41a564f46e2197404b474e6b62f0fe408b8e81
peonso <peonso@hotmail.com>
2016-07-25 16:33:22 -0400

    Fixes for last commits
    bug with doBroadcastMessage
    https://github.com/TwistedScorpio/OTHire/commit/f9c95394717cc924480547ec6c43b129251fc083
    armor should reduce damage
    https://github.com/TwistedScorpio/OTHire/commit/e5029325e4d38225f52743542389f08cf33c9ade

--- data/weapons/scripts/explosive_arrow.lua
+++ data/weapons/scripts/explosive_arrow.lua
@@ -1,4 +1,5 @@
 local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, 1)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_BURSTARROW)
