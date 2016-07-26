556795c30c970dafebcff944ca215c84bfeb4426
peonso <peonso@hotmail.com>
2016-07-26 19:07:34 -0400

    Typo from parent commit

--- source/protocolgame.cpp
+++ source/protocolgame.cpp
@@ -640,7 +640,7 @@ void ProtocolGame::parsePacket(NetworkMessage &msg)
 
 	default:
 		std::cout << "Unknown packet header: " << std::hex << (int)recvbyte << std::dec << ", player " << player->getName() << std::endl;
-		disconnectClient(0x14, "Unknow packet sent.");
+		disconnectClient(0x14, "Unknown packet sent.");
 		break;
 	}
 
