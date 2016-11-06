0099d3e8d5060ed2d2f8db9bef349d9d723ac0e2
peonso <peonso@hotmail.com>
2016-11-06 15:24:38 -0400

    update mysql schema

--- MySql_Schema.sql
+++ MySql_Schema.sql
@@ -93,15 +93,6 @@ CREATE TABLE `environment_killers` (
   PRIMARY KEY (`kill_id`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `environment_killers`
---
-
-INSERT INTO `environment_killers` (`kill_id`, `name`) VALUES
-(6, 'a orc'),
-(18, 'a rat'),
-(19, 'a rat');
-
 -- --------------------------------------------------------
 
 --
@@ -237,67 +228,6 @@ CREATE TABLE `houses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `houses`
---
-
-INSERT INTO `houses` (`id`, `townid`, `name`, `rent`, `guildhall`, `tiles`, `doors`, `beds`, `owner`, `paid`, `clear`, `warnings`, `lastwarning`) VALUES
-(1, 1, 'Main Street 2a', 1500, 0, 40, 1, 2, 0, 0, 0, 0, 0),
-(2, 1, 'Main street 1a', 1500, 0, 38, 1, 2, 0, 0, 0, 0, 0),
-(3, 1, 'Main street 1b', 1500, 0, 40, 1, 2, 0, 0, 0, 0, 0),
-(4, 1, 'Main street 1c', 1500, 0, 40, 1, 2, 0, 0, 0, 0, 0),
-(5, 1, 'Main Street 2b', 1500, 0, 40, 1, 2, 0, 0, 0, 0, 0),
-(6, 1, 'Main Street 2c', 1500, 0, 40, 1, 2, 0, 0, 0, 0, 0),
-(7, 1, 'Shop 1', 1100, 0, 44, 2, 1, 0, 0, 0, 0, 0),
-(8, 1, 'Boat Stream 1', 1200, 0, 28, 1, 1, 0, 0, 0, 0, 0),
-(9, 1, 'Main street 3a', 1500, 0, 31, 1, 1, 0, 0, 0, 0, 0),
-(10, 1, 'Main street 3b', 1500, 0, 32, 1, 1, 0, 0, 0, 0, 0),
-(11, 1, 'Main street 3d', 1500, 0, 31, 1, 1, 0, 0, 0, 0, 0),
-(12, 1, 'Main street 3c', 1500, 0, 32, 1, 1, 0, 0, 0, 0, 0),
-(13, 1, 'Boat Stream 2', 1200, 0, 22, 1, 1, 0, 0, 0, 0, 0),
-(14, 1, 'Shop 2a', 600, 0, 14, 1, 1, 0, 0, 0, 0, 0),
-(15, 1, 'Shop 2b', 900, 0, 19, 1, 1, 0, 0, 0, 0, 0),
-(16, 1, 'Shop 2c', 1000, 0, 17, 1, 1, 0, 0, 0, 0, 0),
-(17, 1, 'Shop 2d', 1000, 0, 18, 1, 1, 0, 0, 0, 0, 0),
-(18, 1, 'Sea View 1', 1300, 0, 36, 1, 1, 0, 0, 0, 0, 0),
-(19, 1, 'Sea View 2', 1300, 0, 36, 1, 1, 0, 0, 0, 0, 0),
-(20, 1, 'Pleasant Home 1', 1100, 0, 36, 1, 2, 0, 0, 0, 0, 0),
-(21, 1, 'Pleasant Home 2', 1100, 0, 33, 1, 2, 0, 0, 0, 0, 0),
-(22, 1, 'Smally Wally 1', 400, 0, 10, 1, 1, 0, 0, 0, 0, 0),
-(23, 1, 'Smally Wally 2', 400, 0, 10, 1, 1, 0, 0, 0, 0, 0),
-(24, 1, 'Smally Wally 3', 400, 0, 10, 1, 1, 0, 0, 0, 0, 0),
-(25, 1, 'Smally Wally 4', 400, 0, 10, 1, 1, 0, 0, 0, 0, 0),
-(26, 1, 'Tharkasian Road 1', 900, 0, 18, 1, 1, 0, 0, 0, 0, 0),
-(27, 1, 'Tharkasian Road 2', 1300, 0, 30, 1, 2, 0, 0, 0, 0, 0),
-(28, 1, 'Tharkas Guildhall 2', 7200, 0, 383, 17, 18, 0, 0, 0, 0, 0),
-(29, 1, 'Tharkas Guildhall 1', 5400, 0, 201, 10, 11, 0, 0, 0, 0, 0),
-(30, 1, 'Tharkas Farm 1', 1350, 0, 33, 2, 1, 0, 0, 0, 0, 0),
-(31, 1, 'Tharkas Farm 2', 1350, 0, 33, 2, 1, 0, 0, 0, 0, 0),
-(32, 2, 'Camel Avenue 1', 1200, 0, 50, 1, 1, 0, 0, 0, 0, 0),
-(33, 2, 'Camel Avenue 2', 800, 0, 41, 1, 1, 0, 0, 0, 0, 0),
-(34, 2, 'Camel Avenue 3', 1200, 0, 55, 2, 1, 0, 0, 0, 0, 0),
-(35, 2, 'Camel Avenue 4', 500, 0, 21, 1, 1, 0, 0, 0, 0, 0),
-(36, 2, 'Camel Avenue 5', 500, 0, 21, 1, 1, 0, 0, 0, 0, 0),
-(37, 2, 'Camel Avenue 6', 500, 0, 21, 1, 1, 0, 0, 0, 0, 0),
-(38, 2, 'Camel Avenue 7', 500, 0, 21, 1, 1, 0, 0, 0, 0, 0),
-(39, 2, 'Camel Avenue 8', 500, 0, 21, 1, 1, 0, 0, 0, 0, 0),
-(40, 2, 'Camel Avenue 9', 500, 0, 21, 1, 1, 0, 0, 0, 0, 0),
-(41, 2, 'Camel Avenue 10', 2000, 0, 84, 1, 2, 0, 0, 0, 0, 0),
-(42, 2, 'Oasis Road 1', 2100, 0, 78, 1, 2, 0, 0, 0, 0, 0),
-(43, 2, 'Oasis Road 2', 2100, 0, 86, 1, 2, 0, 0, 0, 0, 0),
-(44, 2, 'Oasis Road 3', 2100, 0, 82, 1, 2, 0, 0, 0, 0, 0),
-(45, 2, 'Oasis Guildhall', 6000, 0, 229, 6, 10, 0, 0, 0, 0, 0),
-(46, 2, 'Oasis Road 4', 1600, 0, 79, 2, 3, 0, 0, 0, 0, 0),
-(47, 2, 'Oasis Road 5', 1700, 0, 93, 2, 2, 0, 0, 0, 0, 0),
-(48, 2, 'Oasis Road 6', 1400, 0, 71, 1, 1, 0, 0, 0, 0, 0),
-(49, 2, 'Oasis Road 7', 900, 0, 40, 1, 1, 0, 0, 0, 0, 0),
-(50, 2, 'Oasis Road 8', 900, 0, 49, 1, 1, 0, 0, 0, 0, 0),
-(51, 2, 'Oasis Road 9', 900, 0, 51, 1, 1, 0, 0, 0, 0, 0),
-(52, 2, 'Oasis Road 10', 900, 0, 44, 1, 1, 0, 0, 0, 0, 0),
-(53, 2, 'Barrancas Street 1', 1100, 0, 55, 0, 2, 0, 0, 0, 0, 0),
-(54, 2, 'Barrancas Street 2', 1650, 0, 67, 0, 2, 0, 0, 0, 0, 0),
-(55, 2, 'Barrancas Street 3', 850, 0, 43, 0, 1, 0, 0, 0, 0, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -342,28 +272,6 @@ CREATE TABLE `killers` (
   PRIMARY KEY (`id`),
   KEY `death_id` (`death_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
-
---
--- Dumping data for table `killers`
---
-
-INSERT INTO `killers` (`id`, `death_id`, `final_hit`) VALUES
-(2, 2, 1),
-(3, 3, 1),
-(6, 6, 1),
-(7, 7, 1),
-(8, 7, 0),
-(9, 7, 0),
-(10, 8, 1),
-(11, 8, 0),
-(12, 9, 1),
-(13, 10, 1),
-(14, 11, 1),
-(15, 12, 1),
-(16, 12, 0),
-(17, 13, 1),
-(18, 14, 1),
-(19, 14, 0);
 
 -- --------------------------------------------------------
 
@@ -438,10 +346,7 @@ CREATE TABLE `players` (
 --
 
 INSERT INTO `players` (`id`, `name`, `account_id`, `group_id`, `sex`, `vocation`, `experience`, `level`, `maglevel`, `health`, `healthmax`, `mana`, `manamax`, `manaspent`, `soul`, `direction`, `lookbody`, `lookfeet`, `lookhead`, `looklegs`, `looktype`, `posx`, `posy`, `posz`, `cap`, `lastlogin`, `lastlogout`, `lastip`, `save`, `conditions`, `skull_type`, `skull_time`, `loss_experience`, `loss_mana`, `loss_skills`, `loss_items`, `loss_containers`, `town_id`, `balance`, `online`, `rank_id`, `guildnick`) VALUES
-(3, 'GM Ezzz', 123456, 5, 1, 0, 100, 2, 4, 100, 100, 100, 100, 70775, 0, 2, 10, 10, 10, 10, 75, 32368, 32215, 7, 10000, 1396046551, 1396045859, 400558280, 1, '', 0, 0, 100, 100, 100, 10, 100, 1, 0, 1, 0, ''),
-(5, 'Eternal', 123456, 1, 0, 1, 15694800, 100, 100, 100, 100, 1000, 100, 0, 0, 3, 114, 95, 76, 114, 139, 32378, 32108, 7, 500, 1395972211, 1395972205, 16777343, 1, '', 0, 0, 100, 100, 100, 10, 100, 1, 0, 0, 0, ''),
-(6, 'Noob', 123456, 1, 0, 1, 0, 100, 0, 100, 100, 1000, 100, 4294967295, 0, 0, 69, 95, 78, 97, 136, 32369, 32241, 7, 500, 1395972171, 1395971878, 16777343, 1, '', 0, 0, 100, 100, 100, 10, 100, 1, 0, 0, 0, ''),
-(7, 'Third', 123456, 1, 0, 1, 14316087, 97, 0, 85, 85, 10, 10, 18, 0, 2, 69, 95, 78, 97, 136, 32369, 32241, 7, 470, 1396045859, 1396045432, 16777343, 1, '', 4, 1395971878, 100, 100, 100, 10, 100, 1, 0, 0, 0, '');
+(3, 'GM Sample', 123456, 5, 1, 0, 100, 2, 4, 100, 100, 100, 100, 70775, 0, 2, 10, 10, 10, 10, 75, 32368, 32215, 7, 10000, 1396046551, 1396045859, 400558280, 1, '', 0, 0, 100, 100, 100, 10, 100, 1, 0, 1, 0, ''),
 
 --
 -- Triggers `players`
@@ -486,23 +391,6 @@ CREATE TABLE `player_deaths` (
   KEY `date` (`date`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
---
--- Dumping data for table `player_deaths`
---
-
-INSERT INTO `player_deaths` (`id`, `player_id`, `date`, `level`) VALUES
-(2, 5, 1395905164, 1),
-(3, 5, 1395907395, 1),
-(6, 5, 1395959287, 1),
-(7, 6, 1395971072, 1),
-(8, 6, 1395971409, 1),
-(9, 6, 1395971878, 1),
-(10, 5, 1395971910, 1),
-(11, 7, 1395971912, 1),
-(12, 7, 1395971961, 1),
-(13, 7, 1395972261, 100),
-(14, 7, 1395988425, 98);
-
 -- --------------------------------------------------------
 
 --
@@ -518,15 +406,6 @@ CREATE TABLE `player_depotitems` (
   `attributes` blob NOT NULL,
   UNIQUE KEY `player_id` (`player_id`,`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `player_depotitems`
---
-
-INSERT INTO `player_depotitems` (`player_id`, `sid`, `pid`, `itemtype`, `count`, `attributes`) VALUES
-(3, 101, 0, 2589, 1, ''),
-(3, 102, 101, 2594, 1, ''),
-(3, 103, 102, 2148, 40, 0x0f28);
 
 -- --------------------------------------------------------
 
@@ -552,10 +431,6 @@ INSERT INTO `player_items` (`player_id`, `sid`, `pid`, `itemtype`, `count`, `att
 (3, 101, 5, 2152, 98, 0x0f62),
 (3, 102, 6, 2183, 1, ''),
 (3, 103, 8, 2643, 1, ''),
-(5, 101, 1, 2480, 1, ''),
-(5, 102, 3, 1987, 1, ''),
-(5, 103, 8, 2195, 1, ''),
-(7, 101, 3, 1987, 1, '');
 
 -- --------------------------------------------------------
 
@@ -570,23 +445,6 @@ CREATE TABLE `player_killers` (
   PRIMARY KEY (`kill_id`,`player_id`),
   KEY `player_id` (`player_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `player_killers`
---
-
-INSERT INTO `player_killers` (`kill_id`, `player_id`, `unjustified`) VALUES
-(7, 5, 1),
-(8, 7, 1),
-(9, 3, 0),
-(10, 7, 1),
-(11, 5, 1),
-(12, 7, 1),
-(13, 3, 0),
-(14, 3, 0),
-(15, 6, 0),
-(16, 3, 0),
-(17, 5, 0);
 
 -- --------------------------------------------------------
 
@@ -614,27 +472,6 @@ INSERT INTO `player_skills` (`player_id`, `skillid`, `value`, `count`) VALUES
 (3, 4, 10, 0),
 (3, 5, 10, 0),
 (3, 6, 10, 0),
-(5, 0, 11, 21),
-(5, 1, 10, 0),
-(5, 2, 10, 0),
-(5, 3, 10, 0),
-(5, 4, 10, 0),
-(5, 5, 10, 0),
-(5, 6, 10, 0),
-(6, 0, 10, 0),
-(6, 1, 10, 0),
-(6, 2, 10, 0),
-(6, 3, 10, 0),
-(6, 4, 10, 0),
-(6, 5, 10, 0),
-(6, 6, 10, 0),
-(7, 0, 10, 37),
-(7, 1, 10, 0),
-(7, 2, 10, 0),
-(7, 3, 10, 0),
-(7, 4, 10, 0),
-(7, 5, 10, 0),
-(7, 6, 10, 0);
 
 -- --------------------------------------------------------
 
@@ -660,21 +497,6 @@ CREATE TABLE `player_storage` (
   `value` int(11) NOT NULL,
   KEY `player_id` (`player_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `player_storage`
---
-
-INSERT INTO `player_storage` (`player_id`, `key`, `value`) VALUES
-(6, 25000, 1),
-(6, 1000000, 1),
-(6, 2000000, 1),
-(5, 25000, 1),
-(5, 1000000, -1),
-(5, 2000000, -1),
-(7, 25000, 1),
-(7, 1000000, -1),
-(7, 2000000, -1);
 
 -- --------------------------------------------------------
 
