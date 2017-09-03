2fd69340f09d3bcaf76db207ae419e8f9ed99fd1
peonso <peonso@gmail.com>
2017-09-02 20:07:20 -0400

    fixed challenge spell giving skulls
    closes #164
    
    fixed challenge spell being aggressive
    fixed boardsword in items.xml

--- data/spells/scripts/support/challenge.lua
+++ data/spells/scripts/support/challenge.lua
@@ -1,5 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 
 local area = createCombatArea(AREA_SQUARE1X1)
 setCombatArea(combat, area)
